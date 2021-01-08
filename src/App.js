ef4e9dba1167d1f52119a7dbece28cbf2cac6c6f
kai-blt <blaitong@gmail.com>
2021-01-08 15:00:10 -0800

    updated home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+       <Home />
       </header>
     </div>
   );
