ffad882c2e73a85036c685b831590107856c486b
kai-blt <blaitong@gmail.com>
2021-03-09 13:58:06 -0800

    fixes formatting issues home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,10 +7,10 @@ function Home(props) {
   return(
   <div>    
     <div>
-    <p>A service for you to store all of your favorite recipes</p>
+      <p>A service for you to store all of your favorite recipes</p>
     </div>
     <section>
-    <Login setIsLoggedIn={setIsLoggedIn}/>
+      <Login setIsLoggedIn={setIsLoggedIn}/>
     </section>
   </div>
   );
