0cda39e0f31bb91aca8bb7250c9f3de01f669182
kai-blt <blaitong@gmail.com>
2021-03-09 14:04:12 -0800

    moves styles in RecipeView.js to the bottom of the file

--- src/components/recipe/RecipeView.js
+++ src/components/recipe/RecipeView.js
@@ -8,107 +8,6 @@ import styled from 'styled-components';
 import AddRecipeForm from './AddRecipeForm';
 
 
-
-
-const RecipeContainer = styled.div`
-  display: flex;
-  flex-flow: row nowrap;
-  justify-content: space-between;   
-  align-items: flex-start;
-  width: 100%;
-  border-radius: 10px;
-  background: #222;
-  box-shadow: inset 4px 4px 10px #111;
-   
-
-  .desktoptoggle {
-    display: initial;
-  }
-  .mobiletoggle {
-    display: none;
-  }
-
-  @media (max-width: 600px) {
-    .desktoptoggle {
-      display: none;
-    }
-    .mobiletoggle {
-      display: initial;
-    }
-  }
-`;
-
-const SearchNav = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: flex-end;
-  flex-flow: row wrap;
-  margin-bottom: 6%;
-
-  button {
-    margin-bottom: 0;
-  }
-  
-  div {
-    width: 40%;
-  }
-
-  @media (max-width: 600px) {
-    div {
-      width: 100%;
-    }
-  }
-`;  
-
-const RadioToggle = styled.div`
-  display: inline-flex;
-  flex-direction: row-reverse;
-  flex-wrap: nowrap;
-  margin-top: 1%;
-  margin-bottom: 4%;
-  input, label {
-    margin-right: 2px;
-    width: 20px;
-  }
-`;
-
-const RecipeListPane = styled.div`
-  width: 50%;
-  padding-top: 4%;
-  padding-left: 8%;
-  padding-right: 8%;
-  padding-bottom: 50%;
-  padding-bottom: 50%;
-  border-right: 1px dashed #888;
-  @media (max-width: 600px) {
-    width: 100%;
-  }
-`;
-
-const RecipeDirectionsPane = styled.div`
-  width: 50%;
-  padding-top: 4%;
-  padding-left: 8%;
-  padding-right: 8%;
-  padding-bottom: 50%;
-  /* border-left: 1px dashed #888; */
-  @media (max-width: 600px) {
-    width: 0;
-  }
-`;
-
-const ButtonContainer = styled.div`
-  display: flex;   
-  justify-content: space-between;
-  margin-bottom: 4%;
-`;
-
-const Spinner = styled.img`
-  width: 10%;
-`;
-
-
-
 function RecipeView(props) {
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState('');
@@ -295,6 +194,105 @@ function RecipeView(props) {
         </RecipeListPane>     
       </RecipeContainer>
   );
-}
+};
+
+
+//Component Styles
+const RecipeContainer = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;   
+  align-items: flex-start;
+  width: 100%;
+  border-radius: 10px;
+  background: #222;
+  box-shadow: inset 4px 4px 10px #111;
+   
+
+  .desktoptoggle {
+    display: initial;
+  }
+  .mobiletoggle {
+    display: none;
+  }
+
+  @media (max-width: 600px) {
+    .desktoptoggle {
+      display: none;
+    }
+    .mobiletoggle {
+      display: initial;
+    }
+  }
+`;
+
+const SearchNav = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+  flex-flow: row wrap;
+  margin-bottom: 6%;
+
+  button {
+    margin-bottom: 0;
+  }
+  
+  div {
+    width: 40%;
+  }
+
+  @media (max-width: 600px) {
+    div {
+      width: 100%;
+    }
+  }
+`;  
+
+const RadioToggle = styled.div`
+  display: inline-flex;
+  flex-direction: row-reverse;
+  flex-wrap: nowrap;
+  margin-top: 1%;
+  margin-bottom: 4%;
+  input, label {
+    margin-right: 2px;
+    width: 20px;
+  }
+`;
+
+const RecipeListPane = styled.div`
+  width: 50%;
+  padding-top: 4%;
+  padding-left: 8%;
+  padding-right: 8%;
+  padding-bottom: 50%;
+  padding-bottom: 50%;
+  border-right: 1px dashed #888;
+  @media (max-width: 600px) {
+    width: 100%;
+  }
+`;
+
+const RecipeDirectionsPane = styled.div`
+  width: 50%;
+  padding-top: 4%;
+  padding-left: 8%;
+  padding-right: 8%;
+  padding-bottom: 50%;
+  /* border-left: 1px dashed #888; */
+  @media (max-width: 600px) {
+    width: 0;
+  }
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;   
+  justify-content: space-between;
+  margin-bottom: 4%;
+`;
+
+const Spinner = styled.img`
+  width: 10%;
+`;
 
 export default RecipeView;
